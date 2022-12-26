5b3ec376be688139fd0616ff067f72f3e746cb3c
HarshhMonga <hrshmonga@gmail.com>
2022-12-26 12:04:12 +0530

    beta branch added

--- src/MainBranch.java
+++ src/MainBranch.java
@@ -1,5 +1,5 @@
 public class MainBranch {
     public static void main(String[] args) {
-        System.out.println("First Branch");
+        System.out.println("First Branch is now combined with beta");
     }
 }
---
+++ src/beta.java
@@ -0,0 +1,5 @@
+public class beta {
+    public static void main(String[] args) {
+        System.out.println("Beta Branch added");
+    }
+}
